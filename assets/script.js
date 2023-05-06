8ececf6010b11a22d9538de160ffa5091279abad
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-05-05 23:23:30 -0400

    fixed my timer because wasnt working properly and wouldve flooded system

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,30 @@
 
 let countdown = document.getElementById("timeleft");
 let startquiz = document.getElementById("startquizbutton")
-
+let divarea = document.getElementsByTagName("div")
+let strtthequiz = document.getElementById("strtquiz");
+let quest1 = document.getElementById("firstq");
+let quest2 = document.getElementById("secondq");
+let quest3 = document.getElementById("thirdq");
+let quest4 = document.getElementById("fourthq");
 //This is my little countdown timer
-let timer = 75;
+let timer = 60;
 
 // Unfortunately it starts when the page loads but I want to add an event listener so that it will only start when I press Start Quiz 
 startquiz.addEventListener('click', function() {
-    setInterval(function() {
-        let timeLeft = timer--;
-        if(timer >= 0);{
-            timeLeft;
+    strtthequiz.setAttribute("style","display:none");
+    setInterval(function timers() {
+        let timeLeft = timer;
+        if(timeLeft > 0){
+            timer --;
             countdown.textContent = timer;
-        } if (timer <= 0){
-            countdown.textContent = '';
-            clearInterval(downtimer);
+        } if (timeLeft === 0){
+            clearInterval(timers);
         }
+        console.log(timer);
     }, 1000);
-  });
+});
+
+
+
+  
