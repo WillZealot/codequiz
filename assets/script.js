9cacba549a75bf8d45b620df4b1d34e4917e3161
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-05-05 20:53:54 -0400

    added the coutndown timer to an event lsitener

--- assets/script.js
+++ assets/script.js
@@ -5,16 +5,16 @@ let startquiz = document.getElementById("startquizbutton")
 //This is my little countdown timer
 let timer = 75;
 
-let downtimer = setInterval(function() {
-    let timeLeft = timer--;
-    if(timer >= 0);{
-        timeLeft;
-        countdown.textContent = timer;
-    } if (timer <= 0){
-        countdown.textContent = '';
-        clearInterval(downtimer);
-    }
-}, 1000);
-
 // Unfortunately it starts when the page loads but I want to add an event listener so that it will only start when I press Start Quiz 
-
+startquiz.addEventListener('click', function() {
+    setInterval(function() {
+        let timeLeft = timer--;
+        if(timer >= 0);{
+            timeLeft;
+            countdown.textContent = timer;
+        } if (timer <= 0){
+            countdown.textContent = '';
+            clearInterval(downtimer);
+        }
+    }, 1000);
+  });
