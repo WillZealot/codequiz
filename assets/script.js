c2a83ac91b72616f58b9fe41e83a3dbc39224625
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-05-09 16:07:50 -0400

    delelted a ton of java and redid some of the styling and organizing my java so its more understandable 'insert thumbs up emoji here'

--- assets/script.js
+++ assets/script.js
@@ -1,155 +1,103 @@
+
+let startEl = document.getElementById("startquizbutton");
 let countdown = document.getElementById("timeleft");
-let startquiz = document.getElementById("startquizbutton")
-let strtthequiz = document.getElementById("strtquiz");
-let quizData = document.querySelector("quizdata");
 
-let question = document.getElementById("quizQuestion");
+let questAnswEl = document.getElementById("quizdata");
 
-let options = document.getElementsByTagName("button")
-let answer1 = document.getElementById("button1");
-let answer2 = document.getElementById("button2");
-let answer3 = document.getElementById("button3");
-let answer4 = document.getElementById("button4");
-let initialsForm = document.querySelector(".hideform")
+let formEl = document.getElementById("form");
 
-let allAnswers = [answer1,answer2,answer3,answer4];
-
+let introEl = document.getElementById("strtquiz");
+/////////////////////////////////////////////////////////////////
 let timer = 60;
-
+function countdownEl(){
+  setInterval(function timers() {
+    let timeLeft = timer;
+    if(timeLeft > 0){
+        timer --;
+        countdown.textContent = timer;
+    } if (timeLeft === 0){
+        clearInterval(timers);
+    }
+    console.log(timer);
+}, 1000);
+}
+//////////////////////////////////////////////////////////////////
 function hideQuestionAndAnswers() {
-    question.setAttribute("style", "display:none");
-    for (let i = 0; i < allAnswers.length; i++) {
-      allAnswers[i].style.display = "none";
-    }
-  }
-
-  function showQuestionAndAnswers() {
-    question.setAttribute("style", "display:");
-    for (let i = 0; i < allAnswers.length; i++) {
-      allAnswers[i].style.display = "";
-    }
-  }
-
-
-//Basically on screen load right here its going to emmeditelly hide all of the questions buttons and the form
+  questAnswEl.setAttribute("style", "display:none");
+}
+/////////////////////////////////////////////////////////////////
+function showQuestionAndAnswers() {
+  questAnswEl.setAttribute("style", "display:");
+}
+////////////////////////////////////////////////////////////////
+function hideForm(){
+  formEl.setAttribute("style", "display:none")
+}
+//////////////////////////////////////////////////////////////////
+function showForm(){
+  formEl.setAttribute("style", "display:")
+}
+//////////////////////////////////////////////////////////////////
+function hideStrt(){
+  introEl.setAttribute("style", "display:none")
+}
+//////////////////////////////////////////////////////////////////
+//Onload funtion hides some elements I dont want the user to see
 window.onload = function() {
-    //the first for loop hides all the buttons as well as hiding the questions itself
-    question.setAttribute("style", "display:none");
-    hideQuestionAndAnswers();
-
-    
-    initialsForm.setAttribute("style", "display:none");
-    
-    console.log("elements hidden");
-  };
-
-
-startquiz.addEventListener('click', function() {
-
-    strtthequiz.setAttribute("style","display:none");
-    showQuestionAndAnswers();
-
-    setInterval(function timers() {
-        let timeLeft = timer;
-        if(timeLeft > 0){
-            timer --;
-            countdown.textContent = timer;
-        } if (timeLeft === 0){
-            clearInterval(timers);
-        }
-        console.log(timer);
-    }, 1000);
+  hideQuestionAndAnswers();
+  hideForm();
+}
+/////////////////////////////////////////////////////////////////
+startEl.addEventListener('click', function() {
+  countdownEl();
+  showQuestionAndAnswers();
+  hideStrt();
 });
+
+let allAnswers = [0];
 
 let questions = [
     {
-      question1: "Which one changes the text?",
-      answers1: [".textContent", "setAttribute()", "splice()", "Answer 4"],
-      correctAnswer1: ".textContent"
+      question: "Which one changes the text?",
+      answers: [
+        {text: "", correct: "false"},
+        {text: "", correct: "false"},
+        {text: "", correct: "false"},
+        {text: "", correct: "false"}
+      ],
     },
     {
-      question2: "What is the is the name of a true or false valued variable ?",
-      answers2: ["slap", "java", "boolean", "spoof"],
-      correctAnswer2: "boolean"
+      question: "What is the is the name of a true or false valued variable ?",
+      answers: [
+        {text: "", correct: "false"},
+        {text: "", correct: "false"},
+        {text: "", correct: "false"},
+        {text: "", correct: "false"}
+      ],
     },
     {
-        question3: "What is the is the name of a true or false valued variable ?",
-        answers3: ["slap", "java", "boolean", "spoof"],
-        correctAnswer3: "boolean"
+        question: "What is the is the name of a true or false valued variable ?",
+        answers: [
+          {text: "", correct: "false"},
+          {text: "", correct: "false"},
+          {text: "", correct: "false"},
+          {text: "", correct: "false"}
+        ],
       },
       {
-        question3: "What is the is the name of a true or false valued variable ?",
-        answers3: ["slap", "java", "boolean", "spoof"],
-        correctAnswer3: "boolean"
+        question: "What is the is the name of a true or false valued variable ?",
+        answers: [
+          {text: "", correct: "false"},
+          {text: "", correct: "false"},
+          {text: "", correct: "false"},
+          {text: "", correct: "false"}
+        ]
       },
   ];
 
 let currentQuestion = 0;
 
-function firstQuestion(){
 
-}
-
-//to do i need to create a funtion that will set the question element textcontent to question1 in my object array and also change all
-// the buttoins etxt content to answers1 and it also needs to appear after start quiz event is triggered then it needs to target where what they clicked === correct answer1 
-//if not -5 seconds off of the timer if it === then dont remove time from timer then after each question is answerd i need it to display correct or wrong under then after
-// a fewe seconds the next question pops up.
-//after the last question is answered then you need to make the form appear then we can talk buster
-
-
-
-
-
-
-//allAnswers.forEach(function(answer) {
-//    answer.addEventListener('click', function() {
-//        question.setAttribute("style", "display:none");
-//    for (let i = 0; i < allAnswers.length; i++) {
-//        allAnswers[i].style.display = "none";
-//      }
-      // do something here
-//    });
-//  });
-
-// allAnswers.forEach(function(answer) {
-//    answer.addEventListener('click', function() {
-//      question.setAttribute("style", "display:");
-//      for (let i = 0; i < allAnswers.length; i++) {
-//        allAnswers[i].style.display = "";
-//      }
-//     // call a function to update the question and answers
-//      updateQuestionAndAnswers("Which one changes the text", [".textContent", "setAttribute()", "splice()", "Answer 4"]);
-//    });
-//  });
-  
-//  function updateQuestionAndAnswers(newQuestion, newAnswers) {
-    // update the question text
-//    question.textContent = newQuestion;
-    // update each answer text
-//    for (let i = 0; i < allAnswers.length; i++) {
-//      allAnswers[i].textContent = newAnswers[i];
-//   }
-//  }
-
-// allAnswers.forEach(function(answer) {
-//    answer.addEventListener('click', function() {
-//       question.setAttribute("style", "display:");
-//        for (let i = 0; i < allAnswers.length; i++) {
-//          allAnswers[i].style.display = "";
-//       }
-//        // call a function to update the question and answers
-//        updateQuestionAndAnswers("What is the is the name of a true or false valued variable ?", ["slap", "java", "boolean", "spoof"]);
-//      });
-//    });
-//    
-//    function updateQuestionAndAnswers(newQuestion, newAnswers) {
-//      // update the question text
-//      question.textContent = newQuestion;
-//      // update each answer text
-//      for (let i = 0; i < allAnswers.length; i++) {
-//        allAnswers[i].textContent = newAnswers[i];
-//      }
-//    }
   
 
   
